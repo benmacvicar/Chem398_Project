66b82883c9b917f6c2c8c44f5bd522a227af3656
benmacvicar <ben.macvicar@gmail.com>
2019-05-22 13:02:12 -0700

    testing draft 1

--- pumpCom.py
+++ pumpCom.py
@@ -4,13 +4,18 @@ from tkinter import simpledialog
 
 def portCheck(master):
 	
-	prompt = Tk()
 	response = simpledialog.askinteger("Input","What COM port is the pump connected to? (Check Device Manager):", parent = master)
 	return response
 
 def convertToVolumePairs(rate_time_pairs):
+	
 	rate_volume_pairs = list()
 	for pair in rate_time_pairs:
 		vol = pair[0]*pair[1]/60
 		rate_volume_pairs.append(pair[0],vol)
 	return rate_volume_pairs
+
+def sendRun(rate_volume_pairs):
+	
+	for pair in rate_volume_pairs:
+
