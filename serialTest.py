9461dc75836885207014217cb80778f4a8645d59
benmacvicar <35784333+benmacvicar@users.noreply.github.com>
2019-06-12 11:41:29 -0700

    Abbandoning newera package

--- serialTest.py
+++ serialTest.py
@@ -1,9 +1,8 @@
 from new_era import PumpInterface
  
-pi = PumpInterface()
 
 pi = PumpInterface(port = 'COM4',rate_unit='ml/min', volume_unit='ml')
-#pi.xmit_sequence('PH 1','FUN rAtE', 'VOL 3.0', 'RAT 3.0','DIR INF')
-#pi.xmit_sequence('PH 2','FUN rAtE', 'VOL 3.0', 'RAT 3.0','DIR INF')
-pi.xmit_sequence('VER')
+pi._xmit_sequence('PH 1','FUN rAtE', 'VOL 3.0', 'RAT 3.0','DIR INF')
+pi._xmit_sequence('PH 2','FUN rAtE', 'VOL 3.0', 'RAT 6.0','DIR INF')
+#pi._xmit('VER')
 pi.run()
