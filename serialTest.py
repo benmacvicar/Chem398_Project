34f3b3c4b163e4c5fc2f7baa99ce90f02d8b4703
benmacvicar <ben.macvicar@gmail.com>
2019-06-12 10:23:06 -0700

    'testing'

--- serialTest.py
+++ serialTest.py
@@ -2,7 +2,8 @@ from new_era import PumpInterface
  
 pi = PumpInterface()
 
-pi = PumpInterface(port = 3,rate_unit='ml/min', volume_unit='ml')
-pi.xmit_sequence('PH 1','FUN rAtE', 'VOL 3.0', 'RAT 3.0','DIR INF')
-pi.xmit_sequence('PH 2','FUN rAtE', 'VOL 3.0', 'RAT 3.0','DIR INF')
+pi = PumpInterface(port = 4,rate_unit='ml/min', volume_unit='ml')
+#pi.xmit_sequence('PH 1','FUN rAtE', 'VOL 3.0', 'RAT 3.0','DIR INF')
+#pi.xmit_sequence('PH 2','FUN rAtE', 'VOL 3.0', 'RAT 3.0','DIR INF')
+pi.xmit_sequence('VER')
 pi.run()
